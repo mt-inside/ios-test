653c354d0de133a3afa371d0d3f363fce91f0c36
Matt Turner <matturner@gmail.com>
2013-01-23 21:45:38 +0000

    Push the data in the model out to the initial viewControllers when their parent is loaded, not when they're switched to (as the first is never switched to). This feels a bit early to me; shame we don't seem to be able to hook an event when the first one is "switched to"

--- MeetingNotes/TabBarViewController.m
+++ MeetingNotes/TabBarViewController.m
@@ -28,28 +28,6 @@
     return self;
 }
 
-- (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController
-{
-    /* FIXME: this is slightly the wrong place for this, as it only gets called for anything that's not the 1st tab.
-     Both of the child view controllers work fine when they're 2nd tab, as this happens before their viewDidLoad. However if they're 1st it doesn't work.
-     Either need to push the data in to them ahead of the 1st child being made, or
-       on asserting the data into the 1st child, cause it to re-draw (don't think this will work, as the logic where it renders the model lives in viewDidLoad, and we can't cause the thing to be loaded again.
-     */
-    id dvc = viewController;
-    if( [dvc isMemberOfClass:[ActionsViewController class]] )
-    {
-        ActionsViewController *avc = (ActionsViewController *)dvc;
-        avc.actions = [_meetings actions];
-    }
-    else if( [dvc isMemberOfClass:[NavigationViewController class]] )
-    {
-        NavigationViewController *nvc = (NavigationViewController *)dvc;
-        nvc.meetings = _meetings;
-    }
-    
-    return YES;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -59,7 +37,6 @@
     /* This is somewhat akin to an entry point.
      The whole data model is built here and passed through to the following viewControllers which display and edit subsets of it.
      */
-    
     Note *foo = [[Note alloc] init :@"foo" :Action];
     Note *bar = [[Note alloc] init :@"bar" :Feedback];
     
@@ -80,6 +57,22 @@
     [_meetings addMeeting: meetingA];
     [_meetings addMeeting: meetingB];
     [_meetings addMeeting: meetingC];
+    
+    
+    /* "bind" the model into the ViewContollers */
+    for (UIViewController *vc in self.viewControllers)
+    {
+        if( [vc isMemberOfClass:[ActionsViewController class]] )
+        {
+            ActionsViewController *avc = (ActionsViewController *)vc;
+            avc.actions = [_meetings actions];
+        }
+        else if( [vc isMemberOfClass:[NavigationViewController class]] )
+        {
+            NavigationViewController *nvc = (NavigationViewController *)vc;
+            nvc.meetings = _meetings;
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
